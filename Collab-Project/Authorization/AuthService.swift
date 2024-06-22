c5000dad3f30a15867125b28b14485ad236dea50
tataaval <94060509+tataaval@users.noreply.github.com>
2024-06-22 23:52:19 +0400

    add: authorizaton module AGAAAAIN

---
+++ Collab-Project/Authorization/AuthService.swift
@@ -0,0 +1,39 @@
+//
+//  AuthService.swift
+//  Collab-Project
+//
+//  Created by Tatarella on 22.06.24.
+//
+
+import Foundation
+
+enum authError: Error {
+    case userNotFound
+}
+
+extension authError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .userNotFound:
+            return NSLocalizedString("მომხმარებლის მონაცემები არასწორია", comment: "My error")
+        }
+    }
+}
+
+
+class AuthService {
+    
+    private let users: [User] = [
+        User(email: "Avalianitata@gmail.com", password: "123321", balance: 40000)
+    ]
+    
+    func login(email: String, password: String, completion: @escaping (Result<User, Error>) -> Void) {
+        DispatchQueue.global().asyncAfter(deadline: .now() + 3) {
+            if let user = self.users.first(where: { $0.email == email && $0.password == password }) {
+                completion(.success(user))
+            } else {
+                completion(.failure(authError.userNotFound))
+            }
+        }
+    }
+}
---
+++ Collab-Project/Authorization/AuthorizationModel.swift
@@ -0,0 +1,14 @@
+//
+//  AuthorizationModel.swift
+//  Collab-Project
+//
+//  Created by Tatarella on 22.06.24.
+//
+
+import Foundation
+
+struct User: Codable {
+    var email: String
+    var password: String
+    var balance: Int
+}
